78dfa084a1e85b557c3d7aaa2833c4e0fab6af68
therealFIGBERT <naomi@Naomis-MacBook-Air.local>
2019-06-28 16:25:43 -0700

    Finalizing MacOS UI

--- figENC_MacOS.py
+++ figENC_MacOS.py
@@ -9,8 +9,10 @@ canvas = tk.Canvas(root, height=700, width=500)
 canvas.pack()
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=1, relheight=1)
+
 header = tk.Label(frame, text="figENC\nIndustry leading encryption by FIGBERT", bg="gray", fg="black", justify="center", font=("Arial", "18"))
 header.pack(fill="x", side="top", ipady="5")
+
 action = tk.Frame(frame, bg="white")
 action.pack(fill='both')
 action_label = tk.Label(action, text="Action:", bg="white", justify='left', font=("Arial", "14"))
@@ -22,5 +24,24 @@ action_list.insert(3, "Decrypt with generated key")
 action_list.pack(fill='both')
 submit_action = tk.Button(action, text="Begin Process", font=("Arial", "12"))
 submit_action.pack()
+
+modifiers = tk.Frame(frame, bg="white")
+modifiers.pack(fill="both")
+file_label_text = "If you see this, the app broke"
+file_instructions_text = "Separate filepaths with colons (:)"
+file_label = tk.Label(modifiers, text=file_label_text, font=("Arial", "14"))
+file_label.pack()
+file_instructions = tk.Label(modifiers, text=file_instructions_text, font=("Arial", "11"))
+file_instructions.pack()
+file_input = tk.Entry(modifiers, font=("Arial", "12"), justify=tk.LEFT)
+file_input.pack(fill=tk.X)
+passcode_label_text = "If you see this, the app broke"
+passcode_instructions_text = "CRITICAL: DO NOT FORGET YOUR PASSCODE. WITHOUT IT, YOUR DATA IS LOST."
+passcode_label = tk.Label(modifiers, text=passcode_label_text, font=("Arial", "14"))
+passcode_label.pack()
+passcode_instructions = tk.Label(modifiers, text=passcode_instructions_text, font=("Arial", "11"))
+passcode_instructions.pack()
+passcode_input = tk.Entry(modifiers, font=("Arial", "12"), justify=tk.LEFT)
+passcode_input.pack(fill=tk.X)
 
 root.mainloop()
